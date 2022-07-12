97e81da1164a65c04c7f4c18b27ec6eee8f54e86
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-07-12 16:51:22 -0300

    fix: permissions on Movie views

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from utils.mixins import SerializerByMethodMixin
-from utils.permissions import IsSuperUser
+from utils.permissions import IsSuperUser, ReadOnly
 
 from .models import Movie
 from .serializers import ListMoviesSerializer, MovieSerializer
@@ -8,12 +8,23 @@ from .serializers import ListMoviesSerializer, MovieSerializer
 
 class MovieView(SerializerByMethodMixin, generics.ListCreateAPIView):
     queryset = Movie.objects.all()
-    permission_classes = [IsSuperUser]
+    permission_classes = [IsSuperUser | ReadOnly]
     serializers = {"POST": MovieSerializer, "GET": ListMoviesSerializer}
 
 
 class MovieDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    permission_classes = [IsSuperUser]
+    permission_classes = [IsSuperUser | ReadOnly]
     lookup_url_kwarg = "movie_id"
+
+
+class MovieByCinemaView(generics.ListAPIView):
+    serializer_class = MovieSerializer
+
+    def get_queryset(self):
+        # TODO
+        """
+        retornar apenas os Movies que estão sendo exibidos em um Cinema,
+        isto é, os filmes que tem alguma MovieSession agendada
+        """
--- utils/permissions.py
+++ utils/permissions.py
@@ -4,3 +4,8 @@ from rest_framework import permissions
 class IsSuperUser(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_superuser
+
+
+class ReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
