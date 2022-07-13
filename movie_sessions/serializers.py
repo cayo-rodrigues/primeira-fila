2bbf574f6fba9f95394373fce28000f5c7982d0e
Brendo <brendo.n.s.sousa@gmail.com>
2022-07-13 14:58:54 -0300

    fix: add model for movie sessions serializer

--- movie_sessions/serializers.py
+++ movie_sessions/serializers.py
@@ -4,5 +4,6 @@ from .models import MovieSession
 
 class MovieSessionSerializer(serializers.ModelSerializer):
     class Meta:
+        model = MovieSession
         fields = "__all__"
         read_only_fields = ["id", "cinema_id", "room_id", "movie_id"]
