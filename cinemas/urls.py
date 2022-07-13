19ed2329694eeec3ba60d4d153774af34579c8f7
Brendo <brendo.n.s.sousa@gmail.com>
2022-07-13 20:34:14 -0300

    fix: urls for movie sessions moved for cinema urls

--- cinemas/urls.py
+++ cinemas/urls.py
@@ -3,8 +3,26 @@ from rooms.views import CreateRoomView
 
 from .views import CinemaDetailView, CreateCinemaView
 
+from movie_sessions import views
+
 urlpatterns = [
     path("", CreateCinemaView.as_view()),
     path("<cine_id>/", CinemaDetailView.as_view()),
     path("<cine_id>/rooms/", CreateRoomView.as_view()),
+    path(
+        "<cine_id>/room/<room_id>/movie/<movie_id>/movie-sessions/",
+        views.MovieSessionCreateView.as_view(),
+    ),
+    path(
+        "<cine_id>/movie-sessions/",
+        views.MovieSessionCinemaDetailView.as_view(),
+    ),
+    path(
+        "<cine_id>/movies/<movie_id>/movie-sessions/",
+        views.MovieSessionMovieDetailView.as_view(),
+    ),
+    path(
+        "<cine_id>/movies/<movie_id>/movie-sessions/<session_id>/",
+        views.MovieSessionDetail.as_view(),
+    ),
 ]
