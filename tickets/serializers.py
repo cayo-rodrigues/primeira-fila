090535edc7c267448e39070c9a97f8a099f5dee9
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-07-14 19:28:05 -0300

    fix: adjust tickets app to support ticket creation
    - it's still buggy though, because its creating both Seats and SessionSeats, so essensially when someone buyes a ticket he/she is creating a Seat for them on the Room

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -1,48 +1,47 @@
 from movie_sessions.serializers import MovieSessionSerializer
 from rest_framework import serializers
-from rooms.serializers import RoomSerializer
 from users.serializers import UserSerializer
 
 from .models import Seat, SessionSeat, Ticket
 
 
 class SeatSerializer(serializers.ModelSerializer):
-    room = RoomSerializer()
-
     class Meta:
         model = Seat
-        fields = "__all__"
+        fields = ["name"]
 
 
 class SessionSeatSerializer(serializers.ModelSerializer):
     class Meta:
         model = SessionSeat
-        fields = "__all__"
+        exclude = ["ticket"]
+        read_only_fields = ["movie_session"]
 
-    # seat = SeatSerializer()
-    # ticket = TicketSerializer()
-    # session = MovieSessionSerializer()
+    seat = SeatSerializer()
 
 
 class TicketSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     movie_session = MovieSessionSerializer(read_only=True)
-    seats = SessionSeatSerializer(many=True)
+    session_seats = SessionSeatSerializer(many=True)
 
     class Meta:
         model = Ticket
         fields = "__all__"
 
     def create(self, validated_data: dict):
-        seats = validated_data.pop("seats")
+        seats = validated_data.pop("session_seats")
         ticket = Ticket.objects.create(**validated_data)
 
         for session_seat_data in seats:
-            SessionSeat.objects.get_or_create(
-                seat=Seat.objects.get_or_create(**session_seat_data)[0],
+            SessionSeat.objects.create(
+                seat=Seat.objects.create(
+                    name=session_seat_data["seat"]["name"],
+                    room=validated_data["movie_session"].room,
+                ),
                 is_avaliable=False,
+                movie_session=validated_data["movie_session"],
                 ticket=ticket,
             )
-        # ticket = Ticket.objects.create(**seats)
 
         return ticket
