ad9bb02cb21d956c1844147a11cc3b5a7d24574a
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-07-18 09:39:49 -0300

    feat: prevent error on RoomDetailView

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,6 @@
 from cinemas.models import Cinema
-from django.shortcuts import get_object_or_404
 from rest_framework import generics
-from utils.exceptions import CinemaNotFoundError
+from utils.exceptions import CinemaNotFoundError, RoomNotFoundError
 from utils.helpers import safe_get_list_or_404, safe_get_object_or_404
 
 from rooms.models import Room
@@ -18,13 +17,11 @@ class CreateListRoomView(generics.ListCreateAPIView):
         return safe_get_list_or_404(
             self.queryset, CinemaNotFoundError, cinema_id=self.kwargs["cine_id"]
         )
-        # return self.queryset.filter(cinema_id=self.kwargs["cine_id"])
 
     def perform_create(self, serializer):
         cinema = safe_get_object_or_404(
             Cinema, CinemaNotFoundError, pk=self.kwargs["cine_id"]
         )
-        # cinema = get_object_or_404(pk=self.kwargs['cine_id'])
         serializer.save(cinema=cinema)
 
 
@@ -33,9 +30,11 @@ class UpdateRetrieveDeleteRoomView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = RoomSerializer
 
     def get_object(self):
-        queryset = self.get_queryset()
-        obj = get_object_or_404(
-            queryset, pk=self.kwargs["room_id"], cinema_id=self.kwargs["cine_id"]
+        obj = safe_get_object_or_404(
+            Room,
+            RoomNotFoundError,
+            pk=self.kwargs["room_id"],
+            cinema_id=self.kwargs["cine_id"],
         )
 
         return obj
