0723bc5e5fb660bf55f33c351466ee062ebe9de3
Cayo Rodrigues <87717182+cayo-rodrigues@users.noreply.github.com>
2022-07-18 20:00:03 -0300

    fix: remove dupplicated code

--- users/tests/tests_view.py
+++ users/tests/tests_view.py
@@ -135,8 +135,6 @@ class TestUserLoginView(APITestCase):
         cls.user.is_active = True
         cls.user.save()
 
-        cls.user.is_active = True
-        cls.user.save()
 
     def test_login_sucess(self):
         res = self.client.post("/sessions/token/", data=self.login_data, format="json")
