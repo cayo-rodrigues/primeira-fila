545168913f44249731d2176917ec5f7e13ca54ce
Thiago Henrique Monserrat Rezende <thiagohenriquemonserrat@gmail.com>
2022-07-18 14:28:05 -0300

    fix:update of ticket

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -4,7 +4,7 @@ from movie_sessions.serializers import MovieSessionSerializer
 from rest_framework import serializers
 from rooms.models import Seat
 from users.serializers import UserSerializer
-
+import ipdb
 from .models import Ticket
 
 
@@ -60,26 +60,44 @@ class TicketSerializer(serializers.ModelSerializer):
 
 
     def update(self, instance: Ticket, validated_data):
-        seats = validated_data.pop("session_seats")
+        # ipdb.set_trace()
+        sessions_seats = validated_data.pop("session_seats")
+        dado = instance.session_seats.all()
+        
+        for value in dado:
+            value.is_avaliable = True
+            value.save()
+
+        # sessions_seats[1]["seat"] = 'E1'
         chosen_seats = []
+        # ipdb.set_trace()
 
-        for session_seat_data in seats:
-            session_seat_data.is_avaliable = True
-            chosen_seat: SessionSeat = get_object_or_404(
-            SessionSeat,
-            seat = get_object_or_404(
-                Seat,
-                name = session_seat_data["seat"]["name"],
-                room = validated_data["movie_session"].room,
-            ),
-            is_avaliable = True,
-            movie_session = validated_data["movie_session"]
-            )
-            chosen_seat.is_avaliable = False
-            chosen_seat.save()
-            chosen_seats.append(chosen_seat)
+        if len(sessions_seats) == instance.session_seats.count():
 
-        instance.session_seats.set(chosen_seats)
-        instance.save()
-        return instance
+            for session_seat_data in sessions_seats:
+                # session_seat_data["seat"]["name"] = "E1"
+                # ipdb.set_trace()
+                session_seat_data.is_avaliable = True
+                # ipdb.set_trace()
+                chosen_seat: SessionSeat = get_object_or_404(
+                SessionSeat,
+                seat = get_object_or_404(
+                    Seat,
+                    name = session_seat_data["seat"]["name"],
+                    room = validated_data["movie_session"].room,
+                ),
+                is_avaliable = True,
+                movie_session = validated_data["movie_session"]
+                )
+                # ipdb.set_trace()
+                chosen_seat.is_avaliable = False
+                # ipdb.set_trace()
+                chosen_seat.save()
+                # ipdb.set_trace()
+                chosen_seats.append(chosen_seat)
+        # ipdb.set_trace()
+            instance.session_seats.set(chosen_seats)
+        # ipdb.set_trace()
+        # instance.save()
+            return instance
 
