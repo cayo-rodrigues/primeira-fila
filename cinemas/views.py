df0b7a16a1ea4b5be21a6020de5378d391681ea4
Estevão Libardi <estevaolibardi@hotmail.com>
2022-07-19 11:04:09 -0300

    feat: query of cinemas

--- cinemas/views.py
+++ cinemas/views.py
@@ -15,6 +15,33 @@ class CreateCinemaView(SerializeByMethodMixin, generics.ListCreateAPIView):
     queryset = Cinema.objects.all()
     serializer_map = {"GET": ListCinemaSerializer, "POST": CreateCinemaSerializer}
 
+    def get_queryset(self):
+        street = self.request.GET.get("street")
+        district = self.request.GET.get("district")
+        city = self.request.GET.get("city")
+        state = self.request.GET.get("state")
+        country = self.request.GET.get("country")
+
+        if street:
+            self.queryset = Cinema.objects.filter(address__street__iexact=street)
+
+        if district:
+            self.queryset = self.queryset.filter(
+                address__district__name__iexact=district
+            )
+
+        if city:
+            self.queryset = self.queryset.filter(address__city__name__iexact=city)
+
+        if state:
+            self.queryset = self.queryset.filter(address__state__name__iexact=state)
+
+        if country:
+            self.queryset = self.queryset.filter(address__country__name__iexact=country)
+
+        return self.queryset
+        # return super().get_queryset()
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
