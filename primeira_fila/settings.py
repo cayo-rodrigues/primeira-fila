808ebf6084279b818204bcc831fb64f7f932981c
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-07-21 17:34:51 -0300

    fix(v 2.1.8): minor issue with DEBUG setting

--- primeira_fila/settings.py
+++ primeira_fila/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") or True
+DEBUG = os.getenv("DEBUG")
 
 CSRF_TRUSTED_ORIGINS = ["https://primeira-fila.herokuapp.com"]
 
